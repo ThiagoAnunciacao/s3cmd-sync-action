8b4187ffca199e1a372670e976d6c3387b12a5cc
Thiago da Anunciação <thiago.anunciacao@me.com>
2020-11-20 17:47:45 -0300

    debug command unset headers

--- entrypoint.sh
+++ entrypoint.sh
@@ -109,10 +109,10 @@ main() {
         ADD_HEADERS="--add-header=\"$header\" $ADD_HEADERS"
       done
   else
-      ADD_HEADERS=''
+      unset ADD_HEADERS
   fi
 
-  echo $S3CMD_SOURCE_DIR
+  echo $ADD_HEADERS
 
   command="s3cmd --no-preserve $S3CMD_EXCLUDE_FROM $S3CMD_DELETE_REMOVED $ADD_HEADERS $S3CMD_EXTRA_OPTS $S3CMD_CF_INVALIDATE sync ./$S3CMD_SOURCE_DIR/* s3://$AWS_S3_BUCKET"
 
